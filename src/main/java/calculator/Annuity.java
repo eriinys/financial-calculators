e6501ba7b7965ba72071b318153e273fb73fc76e
eriinys <eriinys@duck.com>
2025-09-28 15:32:42 -0500

    .

--- src/main/java/calculator/Annuity.java
+++ src/main/java/calculator/Annuity.java
@@ -4,9 +4,16 @@ import  java.util.*;
 public class Annuity {
     public static void run (Scanner myScanner) {
 
-        int monthlyPay = readInt(myScanner, "Enter the monthly payment amount:");
-        int years = readInt(myScanner, "Enter the years to pay out: ");
-        double interestRate = readDouble(myScanner, "Enter the interest rate:");
+        int monthlyPay = readInt(myScanner, "Enter the desired monthly payout amount:");
+        int years = readInt(myScanner, "Enter the desired years of payout: ");
+        double interestRate = readDouble(myScanner, "Enter the expected interest rate:");
+
+        double desiredPayTotal = monthlyPay * years;
+        double compoundDouble = 72 / interestRate;
+        double presentValue = desiredPayTotal / compoundDouble;
+
+        //output
+        System.out.printf("To fund an annuity that pays $%d monthly for %d years and earns an expected %.2f%% interest, you would need to invest $%.2f today.", monthlyPay, years, interestRate, presentValue);
 
     }
 
