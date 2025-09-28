51e1d904ab884ba4c460c8081030a3407526d657
eriinys <eriinys@duck.com>
2025-09-28 12:57:56 -0500

    .

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Which calculator would you like to use?:\n" +
+                "1 = Mortgage Calculator\n" +
+                "2 = CD Calculator");
+        int choice = input.nextInt();
 
+        if (choice == 1) {
+            MortgageCalculator.run(input);
+        } else if (choice == 2) {
+            CdCalculator.run(input);
+        }
+
+        input.close();
     }
 }
