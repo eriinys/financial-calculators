4e06c648c10f28722b9a75d70ae5ddfa540c7c16
eriinys <eriinys@duck.com>
2025-09-28 15:12:38 -0500

    .

--- src/main/java/calculator/Annuity.java
+++ src/main/java/calculator/Annuity.java
@@ -4,5 +4,18 @@ import  java.util.*;
 public class Annuity {
     public static void run (Scanner myScanner) {
 
+        int monthlyPay = readInt(myScanner, "Enter the monthly payment amount:");
+        int years = readInt(myScanner, "Enter the years to pay out: ");
+        double interestRate = readDouble(myScanner, "Enter the interest rate:");
+
+    }
+
+    public static int readInt(Scanner in, String prompt) {
+        System.out.println(prompt);
+        return  in.nextInt();
+    }
+    public static double readDouble(Scanner in, String prompt) {
+        System.out.println(prompt);
+        return  in.nextDouble();
     }
 }
